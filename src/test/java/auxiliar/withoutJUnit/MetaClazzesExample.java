5fec590a4390d21b4bb170d8b057751634da8c72
Christian Vilca <christian.vilcaq@gmail.com>
2018-11-09 12:58:15 -0500

    Comentarios - METACLASE pruebas sin framework (Sin JUnit)

--- src/test/java/auxiliar/withoutJUnit/MetaClazzesExample.java
+++ src/test/java/auxiliar/withoutJUnit/MetaClazzesExample.java
@@ -8,10 +8,14 @@ public class MetaClazzesExample {
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static void main(String[] args) {
 		try {
+			// Metaclase "Class"
+			// No es un coordinate
+			// Es un objeto que guarda la maquina virtual con la informacion de la clase coordenada
 			Class clazz = Class.forName("ticTacToe.utils.Coordinate");
 			Object object = clazz.getConstructor(int.class, int.class)
 					.newInstance(3, 3);
 			System.out.println("Objeto: " + object);
+			// Obtener el metodo "inMainDiagonal"
 			Method method = clazz.getDeclaredMethod("inMainDiagonal");
 			boolean inMainMethod = (boolean) method.invoke(object);
 			System.out.println("InMainDiagonal: " + inMainMethod);
