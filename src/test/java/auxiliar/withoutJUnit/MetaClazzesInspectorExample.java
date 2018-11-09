21b3eda49846e74cff5174e045724e68c923ae95
Christian Vilca <christian.vilcaq@gmail.com>
2018-11-09 13:19:57 -0500

    REFLEXION - METACLASE pruebas sin framework (Sin JUnit)
    Reflexion -> Escribir codigo Java que maneja codigo Java

--- src/test/java/auxiliar/withoutJUnit/MetaClazzesInspectorExample.java
+++ src/test/java/auxiliar/withoutJUnit/MetaClazzesInspectorExample.java
@@ -5,10 +5,19 @@ import java.io.File;
 public class MetaClazzesInspectorExample {
 
 	public static void main(String[] args){
-		File directory = new File("d:\\Git\\JUnit\\src\\test\\java\\auxiliar\\withoutJUnit");
+		// Se crea un fichero para ver que hay en el disco duro en esta carpeta
+		File directory = new File("C:\\Users\\Christian\\eclipse-workspace\\JUnitC\\src\\test\\java\\auxiliar\\withoutJUnit");
 		String[] clazzes = directory.list();
 		for(String clazz : clazzes) {
 			System.out.println(clazz);
-		}		
+		}	
+//		AnnotationExample.java
+//		AssertExample.java
+//		ClazzExample.java
+//		ClosedIntervalTest.java
+//		CoordenateTest.java
+//		MetaClazzesExample.java
+//		MetaClazzesInspectorExample.java
+//		MetaClazzesWithAnnotationsExample.java
 	}
 }
