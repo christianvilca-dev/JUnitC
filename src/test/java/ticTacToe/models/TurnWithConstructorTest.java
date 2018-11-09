07a4c221d36046172d706ebaaf22144eaeb410d7
Christian Vilca <christian.vilcaq@gmail.com>
2018-11-09 18:42:35 -0500

    Cuerpo - Constructor - para no repetir declaraciones

--- src/test/java/ticTacToe/models/TurnWithConstructorTest.java
+++ src/test/java/ticTacToe/models/TurnWithConstructorTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+// Cuerpo - Constructor - para no repetir declaraciones
 public class TurnWithConstructorTest {
 
 	private Turn OUTTurn;
