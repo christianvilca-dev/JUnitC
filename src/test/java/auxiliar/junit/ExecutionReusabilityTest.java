26912fdfe2a478417a7e3c9f1767265bb4908f9b
Christian Vilca <christian.vilcaq@gmail.com>
2018-11-09 19:18:34 -0500

    Reusabilidad - @Before, @After, @BeforeClass y @AfterClass - Traza

--- src/test/java/auxiliar/junit/ExecutionReusabilityTest.java
+++ src/test/java/auxiliar/junit/ExecutionReusabilityTest.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+// Reusabilidad - @Before, @After, @BeforeClass y @AfterClass - Traza
 public class ExecutionReusabilityTest {
 	
 	private static int staticMember = 0;
@@ -70,4 +71,19 @@ public class ExecutionReusabilityTest {
 	}
 	
 	
+//	static code: 1
+//	before class: 2
+//	constructor: 3 and 1
+//	before: 4 and 2
+//	test 1: 5 and 3
+//	after: 6 and 4
+//	constructor: 7 and 1 -> "1" porque hay nueva instancia de la clase pruebas
+//	before: 8 and 2
+//	test 2: 9 and 3
+//	after: 10 and 4
+//	constructor: 11 and 1
+//	before: 12 and 2
+//	test 3: 13 and 3
+//	after: 14 and 4
+//	after class: 15
 }
